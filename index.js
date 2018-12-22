e64fb5431c80f6d2494b9f744ab68c87fb5c6dac
Aaron Scherer <aequasi@gmail.com>
2018-12-21 18:30:11 -0800

    Trying to fix apply

--- index.js
+++ index.js
@@ -19,7 +19,9 @@ require('twig').cache(process.env.NODE_ENV === 'prod');
 const handler          = serverless(app);
 module.exports.handler = async (event, context) => {
     try {
+        console.log('Initializing Vault');
         await initializeVault(async () => {
+            console.log('Vault Initialized');
             try {
 
                 require('./middleware/session')(app);
@@ -28,7 +30,9 @@ module.exports.handler = async (event, context) => {
                     res.redirect('/');
                 });
                 readdirSync(join(__dirname, './routes')).forEach(file => require(`./routes/${file}`)(app));
+                console.log('Initializing Database');
                 app.database = await initializeDatabase();
+                console.log('Initializing Eris');
                 app.eris     = new Eris('Bot ' + process.secrets.discord.token, {restMode: true});
             } catch (e) {
                 console.error(e);
